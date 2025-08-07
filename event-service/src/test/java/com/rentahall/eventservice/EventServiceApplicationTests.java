1f38973f5a3e132808a70bac41cdccb2e4289aee
uniquesp <sakshisp1342002@gmail.com>
2025-08-07 16:23:51 +0530

    initial commit

---
+++ event-service/src/test/java/com/rentahall/eventservice/EventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentahall.eventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
